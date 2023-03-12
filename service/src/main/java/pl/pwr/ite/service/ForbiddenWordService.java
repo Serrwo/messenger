ff608dcd434caaaa1536dcc2ce45b38f4afbb634
R3VANEK <janek2000-2000@wp.pl>
2023-03-12 23:30:36 +0100

    added censorships of words based on db table ForbiddenWord

--- service/src/main/java/pl/pwr/ite/service/ForbiddenWordService.java
+++ service/src/main/java/pl/pwr/ite/service/ForbiddenWordService.java
@@ -4,10 +4,13 @@ import pl.pwr.ite.model.entity.ForbiddenWord;
 import pl.pwr.ite.model.entity.Message;
 
 import java.util.Iterator;
+import java.util.regex.Pattern;
 
 public interface ForbiddenWordService extends EntityService<ForbiddenWord>{
 
    Iterator<ForbiddenWord> iterate();
 
    Message censorMessage(Message messageToCensor);
+
+   Pattern compileCensorPattern();
 }
--- service/src/main/java/pl/pwr/ite/service/impl/ForbiddenWordServiceImpl.java
+++ service/src/main/java/pl/pwr/ite/service/impl/ForbiddenWordServiceImpl.java
@@ -1,39 +1,84 @@
 package pl.pwr.ite.service.impl;
 
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 import pl.pwr.ite.model.entity.ForbiddenWord;
 import pl.pwr.ite.model.entity.Message;
 import pl.pwr.ite.model.repository.ForbiddenWordRepository;
 import pl.pwr.ite.service.ForbiddenWordService;
 
+import java.util.HashMap;
 import java.util.Iterator;
-import java.util.UUID;
 import java.util.regex.Pattern;
 
 @Service
 public class ForbiddenWordServiceImpl extends EntityServiceBase<ForbiddenWord> implements ForbiddenWordService, InitializingBean {
 
+    private static Pattern censorPattern;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+       censorPattern =  compileCensorPattern();
+    }
+
     protected ForbiddenWordServiceImpl(ForbiddenWordRepository repository) {
         super(repository);
+
     }
 
     public Iterator<ForbiddenWord> iterate(){
           return repository.findAll().iterator();
     }
 
+
     @Override
     public Message censorMessage(Message messageToCensor) {
 
+        messageToCensor.setMessage(messageToCensor.getMessage().replaceAll(String.valueOf(censorPattern), "<censored>"));
 
-
-
-        return null;
+        return messageToCensor;
     }
 
     @Override
-    public void afterPropertiesSet() throws Exception {
+
+    public Pattern compileCensorPattern() {
+        Iterator<ForbiddenWord> iterator = iterate();
+        StringBuilder patternStringBuilder = new StringBuilder();
+        String helper;
+
+        HashMap<String, String> lettersDigitsHashMap = new HashMap<String, String>(){
+            {
+                put("a", "@");
+                put("i", "!");
+                put("e", "3");
+                put("g", "6");
+                put("s", "\\$");
+                put("l", "1");
+                put("t", "7");
+            }
+        };
+
+
+        while(iterator.hasNext()){
+            helper =  iterator.next().getWord();
+
+            for(String key : lettersDigitsHashMap.keySet()){
+                try{
+                    helper = helper.replaceAll(key, "("+key+"|"+lettersDigitsHashMap.get(key)+")");
+                }
+                catch (IllegalArgumentException e){
+                    System.out.println("asd");
+                }
+
+            }
+            patternStringBuilder.append(helper);
+            patternStringBuilder.append("?[^(\\s|\\.)]+|");
+
+        }
+        patternStringBuilder.deleteCharAt(patternStringBuilder.length()-1);
+        return Pattern.compile(String.valueOf(patternStringBuilder), Pattern.CASE_INSENSITIVE);
 
     }
+
+
 }
